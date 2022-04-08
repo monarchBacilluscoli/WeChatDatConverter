2b5dd497f787edc9d6c47e5889e049351a329760
liuyongfeng <531817981@qq.com>
2022-04-09 01:58:15 +0800

    1st commit

---
+++ converter.py
@@ -0,0 +1,36 @@
+# 将微信的dat格式文件转换为图片文件
+
+import os
+
+output_path = "./"
+
+
+def GetFiles(path):
+    files = os.listdir(path)
+    file_list = []
+    for file in files:
+        filename, extension = os.path.splitext(file)
+        if os.path.isfile(os.path.join(path, file)) and extension == ".dat":
+            file_list.append(os.path.join(path, file))
+    return file_list
+
+def imageDecode(f, fn):
+    dat = open(f, "rb")
+    out = output_path + fn + ".png"
+    png = open(out, "wb")
+    for now in dat:
+        for nowByte in now:
+            newByte = nowByte ^ 0x5D  # 注意将这里的二进制数改成dat格式开头的4个16进制值和jpeg格式开头的FF D8的亦或结果的前两个
+            png.write(bytes([newByte]))
+    dat.close()
+    png.close()
+
+
+def DecodeDatInCurPath(onOneFileComplete):
+    # 遍历当前文件夹下的所有文件
+    files = GetFiles(output_path)
+    # 对所有文件调用imageDecode
+    for file in files:
+        print("处理中："+file)
+        imageDecode(file, os.path.basename(file))
+        onOneFileComplete()
---
+++ gui.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+import converter
+
+isConverted = False
+top = tk.Tk()
+convertBtn = tk.Button(text="开始转换")
+curProcess = tk.Label()
+
+totalFileNum = 0
+curCompleteFileNum = 0
+
+# 转换按键回调
+def onConvertButtonClick(event):
+    global isConverted
+
+    print("按下了转换按钮 ", event)
+    if(isConverted == False):
+        isConverted = True
+        converter.DecodeDatInCurPath(onOneFileComplete)
+        convertBtn.config(text="关闭")
+        curProcess.config(text="转换完毕")
+    else:
+        top.destroy()
+
+# 当一个文件转换完成 - 用于更新进度条
+def onOneFileComplete():
+    global curProcess
+    global curCompleteFileNum
+    global totalFileNum
+
+    curCompleteFileNum += 1
+    curProcess.config(text=str(curCompleteFileNum)+"/" + str(totalFileNum))
+    top.update_idletasks()
+
+
+def onGUI():
+    global curProcess
+    global totalFileNum
+    global top
+
+    top.geometry("200x100")
+    top.title("微信dat文件转图片工具")
+
+    totalFileNum = len(converter.GetFiles(converter.output_path))
+    curProcess = tk.Label(text="待转换文件总数: "+str(totalFileNum))
+    curProcess.pack()
+
+    convertBtn.bind("<Button-1>", onConvertButtonClick)
+    convertBtn.pack()
+
+    top.mainloop()
+
+
+onGUI()
